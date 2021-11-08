9d96e1479076825a9bd037e3f6a9eba90991eb29
Henrique Brites <henriquebrites@live.com>
2021-11-08 00:28:26 -0400

    feature: show data with resource mode

--- app/Http/Controllers/Admin/ClientsController.php
+++ app/Http/Controllers/Admin/ClientsController.php
@@ -47,12 +47,12 @@ class ClientsController extends Controller //Controller resource
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $client
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Client $client)
     {
-        //
+        return view('admin.clients.show', compact('client'));
     }
 
     /**
